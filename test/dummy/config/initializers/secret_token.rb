6b34ba8655df6b305fc9a311a78cb6ed9bca571a
Yasunori Suzuki <railsyas@gmail.com>
2013-02-14 21:43:46 +0900

    [Diffferences] + Created mail_form rails plugin.
    [Reasons]
    + To learn about how to build rails plugin.

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '778f151e95bec2a6bbc4e2b29f09589f433e2cfd4a57f7d2634d7943bb7fc60a739fa57cabea33eea26e37e98088c29c40ba90f8307d2aea2ce233b001f1b723'
